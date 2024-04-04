00e3fd366fc44b0c694545c654f0f323936f1d8b
Aifedayo <akeemifedayolag@gmail.com>
2024-04-04 10:00:00 +0100

    Add the imports to scrape amazon

--- scrapy.py
+++ scrapy.py
@@ -1,5 +1,14 @@
-import requests
-import pandas as pd
+import requests # type: ignore
+import pandas as pd # type: ignore
 from bs4 import BeautifulSoup
 
 def scrape_amazon():
+    url = "https://www.amazon.com/s?i=specialty-aps&bbn=16225007011&rh=n%3A16225007011%2Cn%3A1292115011&refresh=2&ref=glow_cls"
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    return soup
+
+if __name__ == "__main__":
+    scrape_amazon()
