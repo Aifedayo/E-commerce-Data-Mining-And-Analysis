2e2a6992c77ac5f71f3afa64471d756d00653e29
Aifedayo <akeemifedayolag@gmail.com>
2024-10-01 22:20:39 +0100

    Adding rating score count

--- scrapy.py
+++ scrapy.py
@@ -1,5 +1,6 @@
 import requests # type: ignore
 import time
+import pprint
 import pandas as pd # type: ignore
 from bs4 import BeautifulSoup
 
@@ -17,6 +18,7 @@ service = Service(executable_path="./chromedriver")
 driver = webdriver.Chrome(service=service)
 url = "https://www.amazon.com/s?i=computers-intl-ship&bbn=16225007011&rh=n%3A1292115011&dc&ds=v1%3AsYkCBCl%2Bka9plXFHMRFlkzFuPcELKHQ9Vt7AQ2GjoiY&qid=1727780297&refresh=2&rnid=85457740011&ref=sr_nr_p_123_1"
 
+printer = pprint.PrettyPrinter()
 
 def scrape_amazon():
     page_class_name = "sg-col-20-of-24"
@@ -25,15 +27,15 @@ def scrape_amazon():
 
 
     tiltle_div_class_name = "a-section a-spacing-none a-spacing-top-small s-title-instructions-style"
-    title_class_name = "a-size-mini"
+    title_class_name = "a-size-base-plus"
 
     item_name_xpath = '//span[contains(@class, "a-size-base-plus a-color-base a-text-normal")]'
     
-    rating_score_xpath = '//span[contains(@class, "a-icon-alt")]'
-    rating_count_xpath = '//span[contains(@class, "a-size-base s-underline-text")]'
+    rating_score_xpath = '//span[contains(@class, a-declarative")]'
+    rating_count_xpath = '//div[contains(@class, "s-csa-instrumentation-wrapper")]'
 
     review_div_class_name = "a-section a-spacing-none a-spacing-top-micro"
-    review_span_class_name_rating = "a-icon-alt"
+    review_span_class_name_rating = "a-icon-star-small"
     review_span_class_name_count = "a-size-base s-underline-text"
 
     price_div_class_name = "a-section a-spacing-none a-spacing-top-small s-price-instructions-style"
@@ -53,24 +55,29 @@ def scrape_amazon():
 
     soup = BeautifulSoup(page, "html.parser")
 
-    span_element = soup.find('span', class_=title_class_name)
-    # print(span_element.text if span_element else "Element not found")
+    driver.get(url)
+
+    rating_score_spans = soup.find_all("span", class_="a-icon-alt")
+    rating_count_spans = soup.find_all("span", class_="a-size-base s-underline-text")
+
+    print(rating_score_spans)
+
     items = []
     
     elements = driver.find_elements(By.XPATH, title_xpath)
-    for element in elements:
+    for i, element in enumerate(elements):
         # Find the title, rating score, and rating count within the current element
-        title = element.find_element(By.CLASS_NAME, title_class_name).text.split(",")
-        # rating_score = element.find_element(By.XPATH, rating_score_xpath).text
-        # rating_count = element.find_element(By.XPATH, rating_count_xpath).text
+        title = element.find_element(By.CLASS_NAME, title_class_name).text
+        rating_score = rating_score_spans[i].text if i < len(rating_score_spans) else "No rating"
+        rating_count = rating_count_spans[i].text if i < len(rating_count_spans) else "0"
+
 
         # Append the extracted data to the items list
-        items.append((title))
-        print(element)
+        items.append((title, rating_score, rating_count))
 
         time.sleep(2)
     
-    print(items)
+    printer.pprint(items)
 
     driver.quit()
 
